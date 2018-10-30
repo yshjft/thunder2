50d75c89ebebd3e42fa9bd564e43670863876853
yshjft <jericoh@naver.com>
2018-10-30 19:50:44 +0900

    Initial commit

---
+++ app/src/main/java/com/example/thunder2/MainActivity.java
@@ -0,0 +1,27 @@
+ package com.example.thunder2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+ public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+     public void onButton3Clicked(View v){
+         Intent intent=new Intent(getApplicationContext(), 대회정보.class);
+         startActivity(intent);
+     }
+
+     public void onButton4Clicked(View v){
+         Intent intent=new Intent(getApplicationContext(), 설정.class);
+         startActivity(intent);
+     }
+
+
+}
