01e40dfff235efbe8788c3516e64280751c42a36
Microsong <jhn06046@hotmail.com>
2018-11-10 00:22:58 +0900

    lodingActivity

---
+++ app/src/main/java/com/example/thunder2/LodingActivity.java
@@ -0,0 +1,21 @@
+package com.example.thunder2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LodingActivity extends Activity {
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_loading);
+        startLoading();
+    }
+    private void startLoading(){
+        Handler handler = new Handler();    //핸들러
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                finish();
+            }
+        },2000);
+    }
+}
