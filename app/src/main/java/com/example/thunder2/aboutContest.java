385e1d8c16c321856e17f8376c4f1b388924417a
yshjft <jericoh@naver.com>
2018-11-15 15:59:54 +0900

    대회에 지도 추가

--- app/src/main/java/com/example/thunder2/aboutContest.java
+++ app/src/main/java/com/example/thunder2/aboutContest.java
@@ -3,12 +3,49 @@ package com.example.thunder2;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class aboutContest extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class aboutContest extends AppCompatActivity
+        implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_contestinformation_aboutcontest);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
     }
+
+    public void onMapReady(final GoogleMap map) {
+        double mLatitude = 37.225165;
+        double mLongitude = 127.187141;
+
+        mMap = map;
+
+        LatLng now = new LatLng(mLatitude, mLongitude);   //현재 위치
+
+        MarkerOptions marketOptions = new MarkerOptions();
+
+        marketOptions.position(now);    //지금 위치로 포지션
+        marketOptions.title("PC방");
+        marketOptions.snippet("레볼루션 PC방");
+        mMap.addMarker(marketOptions);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(now));
+
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
+    }
+
+
 
 }
