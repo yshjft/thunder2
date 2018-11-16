b5fbe57fa1a57096bc2df62bfe34e633f8642706
Microsong <jhn06046@hotmail.com>
2018-11-16 23:40:42 +0900

    GPS 인터넷 체크

--- app/src/main/java/com/example/thunder2/MainActivity.java
+++ app/src/main/java/com/example/thunder2/MainActivity.java
@@ -1,14 +1,18 @@
  package com.example.thunder2;
 
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -37,6 +41,10 @@ import java.util.ArrayList;
          startActivity(intent);
 
 
+         if(!chkGpsService()){
+             Toast.makeText(this,"무선 네트워크 혹은 GPS에 연결되지 않았습니다!", Toast.LENGTH_SHORT).show();
+         }
+
          init();
 
          rcv = (RecyclerView) findViewById(R.id.main_rcv);
@@ -44,8 +52,39 @@ import java.util.ArrayList;
 
          rcvAdapter = new RcvAdapter_forPC(this, list);
          rcv.setAdapter(rcvAdapter);
+     }
+     //GPS 설정 체크
+     private boolean chkGpsService() {
 
+         String gps = android.provider.Settings.Secure.getString(getContentResolver(), android.provider.Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
 
+         Log.d(gps, "aaaa");
+
+         if (!(gps.matches(".*gps.*") && gps.matches(".*network.*"))) {
+
+             // GPS OFF 일때 Dialog 표시
+
+             AlertDialog.Builder gsDialog = new AlertDialog.Builder(this);
+             gsDialog.setTitle("위치 서비스 설정");
+             gsDialog.setMessage("무선 네트워크 사용, GPS 위성 사용을 모두 체크하셔야 정확한 위치 서비스가 가능합니다.\n위치 서비스 기능을 설정하시겠습니까?");
+             gsDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                 public void onClick(DialogInterface dialog, int which) {
+                     // GPS설정 화면으로 이동
+                     Intent intent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                     intent.addCategory(Intent.CATEGORY_DEFAULT);
+                     startActivity(intent);
+                 }
+             })
+                     .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                         public void onClick(DialogInterface dialog, int which) {
+                             return;
+                         }
+                     }).create().show();
+             return false;
+
+         } else {
+             return true;
+         }
      }
 
 
