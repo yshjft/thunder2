fc22883576bed91e71cda41cb414eef759752f1a
Microsong <jhn06046@hotmail.com>
2018-11-17 00:45:12 +0900

    인터넷 GPS

--- app/src/main/java/com/example/thunder2/MainActivity.java
+++ app/src/main/java/com/example/thunder2/MainActivity.java
@@ -1,8 +1,13 @@
  package com.example.thunder2;
 
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.provider.Settings;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -28,7 +33,7 @@ import java.util.ArrayList;
      private boolean isAccessCoarseLocation = false;
      private boolean isPermission = false;
 
-
+     LocationManager locationManager;
      RecyclerView rcv;
      RcvAdapter_forPC rcvAdapter;
      ArrayList<DataForm_forPC> list = new ArrayList<>();
@@ -40,10 +45,43 @@ import java.util.ArrayList;
          Intent intent = new Intent(this, LoadingActivity.class);
          startActivity(intent);
 
-
-         if(!chkGpsService()){
-             Toast.makeText(this,"무선 네트워크 혹은 GPS에 연결되지 않았습니다!", Toast.LENGTH_SHORT).show();
+         if(chkGpsService() == false){
+             Toast.makeText(this,"GPS가 꺼져있습니다.",Toast.LENGTH_LONG).show();
          }
+         else{
+             Toast.makeText(this,"GPS 체크 완료",Toast.LENGTH_LONG).show();
+         }
+
+         ConnectivityManager manager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+         NetworkInfo mobile = manager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+         NetworkInfo wifi = manager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+
+         // wifi 또는 모바일 네트워크 어느 하나라도 연결이 되어있다면,
+         if (wifi.isConnected() || mobile.isConnected()) {
+             Log.i("연결됨" , "연결이 되었습니다.");
+             Toast.makeText(this,"인터넷 체크 완료",Toast.LENGTH_LONG).show();
+         } else {
+             Log.i("연결 안 됨" , "연결이 다시 한번 확인해주세요");
+             Toast.makeText(this,"인터넷을 확인해주세요되었습니다.",Toast.LENGTH_LONG).show();
+             AlertDialog.Builder gsDialog = new AlertDialog.Builder(this);
+             gsDialog.setTitle("인터넷 설정");
+             gsDialog.setMessage("인터넷 기능을 설정하시겠습니까?");
+             gsDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                 public void onClick(DialogInterface dialog, int which) {
+                     // GPS설정 화면으로 이동
+                     Intent intent = new Intent(Settings.ACTION_WIFI_IP_SETTINGS);
+                     intent.addCategory(Intent.CATEGORY_DEFAULT);
+                     startActivity(intent);
+                 }
+             })
+                     .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                         public void onClick(DialogInterface dialog, int which) {
+                             return;
+                         }
+                     }).create().show();
+         }
+
 
          init();
 
@@ -53,7 +91,7 @@ import java.util.ArrayList;
          rcvAdapter = new RcvAdapter_forPC(this, list);
          rcv.setAdapter(rcvAdapter);
      }
-     //GPS 설정 체크
+
      private boolean chkGpsService() {
 
          String gps = android.provider.Settings.Secure.getString(getContentResolver(), android.provider.Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
@@ -63,7 +101,6 @@ import java.util.ArrayList;
          if (!(gps.matches(".*gps.*") && gps.matches(".*network.*"))) {
 
              // GPS OFF 일때 Dialog 표시
-
              AlertDialog.Builder gsDialog = new AlertDialog.Builder(this);
              gsDialog.setTitle("위치 서비스 설정");
              gsDialog.setMessage("무선 네트워크 사용, GPS 위성 사용을 모두 체크하셔야 정확한 위치 서비스가 가능합니다.\n위치 서비스 기능을 설정하시겠습니까?");
@@ -86,6 +123,7 @@ import java.util.ArrayList;
              return true;
          }
      }
+
 
 
      public void onButton_contest(View v) {
