9527e2300d1106a2b09c6cb440f85ab835b56b68
nyangoon <khc104@gmail.com>
2018-11-19 12:38:20 +0900

    SignIn, SignUp 구현

---
+++ app/src/main/java/com/example/thunder2/SignUp.java
@@ -0,0 +1,61 @@
+package com.example.thunder2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUp extends AppCompatActivity {
+
+    private EditText email_join;
+    private EditText pwd_join;
+    private Button btn;
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        email_join = (EditText) findViewById(R.id.sign_up_email);
+        pwd_join = (EditText) findViewById(R.id.sign_up_pwd);
+        btn = (Button) findViewById(R.id.sign_up_btn);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = email_join.getText().toString().trim();
+                String pwd = pwd_join.getText().toString().trim();
+
+                firebaseAuth.createUserWithEmailAndPassword(email, pwd)
+                        .addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (task.isSuccessful()) {
+                                    Intent intent = new Intent(SignUp.this, SignIn.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Toast.makeText(SignUp.this, "등록 에러", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+                            }
+                        });
+            }
+        });
+
+
+    }
+}
